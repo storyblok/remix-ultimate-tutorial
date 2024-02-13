67b8b413d0101d339a8e1f200a6f0cda2711efd1
Alexandra Spalato <alexaspalato@gmail.com>
2024-02-13 11:07:57 +0100

    Refactor language handling in utils file

--- app/utils/langs.jsx
+++ app/utils/langs.jsx
@@ -1,13 +1,16 @@
 import { useLocation } from "@remix-run/react";
 
-//You can have the languages in an utils file so it can be reused.
-let languages = ["en", "es"];
+const languages = ["en", "es"]; // List of languages your app supports
 
 const useCurrentLanguage = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
+  const location = useLocation(); // Get info about the current webpage
+  const currentPath = location.pathname; // Find out the current webpage address
+
+  // Look for a language in the webpage address. If not found, use English.
   const currentLanguage =
     languages.find((lang) => currentPath.startsWith(`/${lang}`)) || "en";
+
+  // Give back the webpage address and the language we found
   return { currentPath, currentLanguage };
 };
 
