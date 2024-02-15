d9bb7bdffa95d3725e3adc504d5718eb6855a5e8
Alexandra Spalato <alexaspalato@gmail.com>
2024-02-15 11:17:29 +0100

    Refactor class to className in Feature component

--- app/components/Feature.jsx
+++ app/components/Feature.jsx
@@ -4,15 +4,15 @@ const Feature = ({ blok }) => {
   return (
     <div
       {...storyblokEditable(blok)}
-      class="w-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden"
+      className="w-full bg-[#f7f6fd] rounded-[5px] text-center overflow-hidden"
     >
       <img
         src={blok.image.filename}
         alt={blok.image.alt}
-        class="w-full h-48 xl:h-72 object-cover"
+        className="w-full h-48 xl:h-72 object-cover"
       />
-      <div class="px-12 py-6">
-        <h3 class="text-2xl text-[#1d243d] font-bold">{blok.name}</h3>
+      <div className="px-12 py-6">
+        <h3 className="text-2xl text-[#1d243d] font-bold">{blok.name}</h3>
       </div>
     </div>
   );
