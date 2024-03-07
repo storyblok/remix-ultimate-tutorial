afab1976eba12af1e28a5595ec1f8f1c6d3910aa
Alexandra Spalato <alexaspalato@gmail.com>
2024-03-07 18:37:07 +0100

    cache

--- app/routes/$.jsx
+++ app/routes/$.jsx
@@ -57,7 +57,7 @@ export const loader = async ({ params, request }) => {
   };
 
   const { data } = await getStoryblokApi()
-    .get(`cdn/stories/${slug}`, sbParams)
+    .get(`cdn/stories/${slug}`, sbParams, { cache: "no-store" })
     .catch((e) => {
       console.log("e", e);
       return { data: null };
@@ -67,12 +67,16 @@ export const loader = async ({ params, request }) => {
     throw new Response("Not Found", { status: 404 });
   }
 
-  let { data: articles } = await getStoryblokApi().get(`cdn/stories`, {
-    version,
-    starts_with: "blog/",
-    language,
-    is_startpage: 0,
-    // cv: Date.now(),
-  });
+  let { data: articles } = await getStoryblokApi().get(
+    `cdn/stories`,
+    {
+      version,
+      starts_with: "blog/",
+      language,
+      is_startpage: 0,
+      // cv: Date.now(),
+    },
+    { cache: "no-store" }
+  );
   return json({ story: data?.story, articles: articles?.stories });
 };
